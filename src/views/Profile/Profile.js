7190168f43986ccb7ba8585a86f0a6d439d2447f
Sofia Damaso <57020694+sofidamaso@users.noreply.github.com>
2023-03-06 17:28:31 +0100

    Create Profile.js

---
+++ src/views/Profile/Profile.js
@@ -0,0 +1,16 @@
+import {React} from "react";
+
+function Profile() {
+    return (
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        )
+
+}
+export default Profile;
